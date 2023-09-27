1960c32ae15ef2750eb6eca1259c7f639e75066e
antonio-dojedul <anavarro67@hotmail.com>
2023-09-27 14:51:20 +0200

    continua con el codigo en js

--- script.js
+++ script.js
@@ -1,39 +1,78 @@
 
 const buttons$$ = document.querySelectorAll('button');
-let result_screen$$ = document.querySelector('.result');
+
+
 numbrer_operators = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
-
+operatos = ["divide", "multiplica", "restar", "sumar"]
+operatos_signs = ["/", "X", "-", "+"]
 
 const operar = (e) => {
     
+    let resultado$$ = document.querySelector('#resultado');
+    let parcial$$ = document.querySelector('#parcial');
+    let resultado = 0
+
 
     boton_pulsado = e.target
-    
+    valor = e.target.innerText
+     
+
+    console.log(boton_pulsado.id)
+    console.log('valor = ', valor)
+    console.log('parcial$$.innerText = ', parcial$$.innerText )
+
+// Botones numericos, los agrega a pantalla.
     if (numbrer_operators.includes(boton_pulsado.id))
     {
-        console.log("He pulsado: ", boton_pulsado.id)
-        result_screen$$.innerText = result_screen$$.innerText + boton_pulsado.id
-    }
-
-    if (boton_pulsado.id == "reset") {
-        console.log("He pulsado reset")
         
-        result_screen$$.innerText = "0";
+        // Si está a cero, limpio la pantalla antes de escribir. 
+        if (resultado$$.innerText == "0") {
+            resultado$$.innerText = ""
+        }
+        
+        // Comprueba si el resultado parcial, contiene algun operador: +, -, x, /. 
+        // Devuelve True si lo contiene. 
+        var contieneOperador = operatos_signs.some(caracter => parcial$$.innerText.includes(caracter));
+
+        if (contieneOperador) {
+            resultado$$.innerText = ""
+        }
+
+        resultado$$.innerText = resultado$$.innerText + boton_pulsado.id
     }
-
-    if (boton_pulsado.id == "reset-act-num") {
-        result_screen$$.innerText = "0";
+// RESET
+    if (boton_pulsado.id == "reset") {       
+        
+        resultado$$.innerText = "0";
+        parcial$$.innerHTML = ""
     }
-    
-    // En funcion del id del boton pulsado realizo una operación u otra.     
-    
-    
+// Cambio de signo. 
+    if (boton_pulsado.id == "sign") {
+        resultado$$.innerText = parseInt(resultado$$.innerText) * -1
+    }
+// Operadores: +, -, x, /.     
+    if (operatos.includes(boton_pulsado.id)) {
 
-    
 
+        var contieneOperador = operatos_signs.some(caracter => parcial$$.innerText.includes(caracter));
 
+        if (contieneOperador) {
+           
+            resultado = eval(parcial$$.innerText + resultado$$.innerHTML)         
+            parcial$$.innerHTML = resultado + boton_pulsado.innerHTML
+
+        } else {
+            parcial$$.innerHTML  = parcial$$.innerHTML = resultado$$.innerHTML + boton_pulsado.innerHTML
+        } 
+        }
+
+        console.log("El resultado es: ", resultado)
+
+        
 }
+
+
+
 
 // Añade la funcion operar a cada boton al hacer click en el 
 for (let button of buttons$$){    
